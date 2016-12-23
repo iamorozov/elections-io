d884ef509e2352f72b65f6ef84082dddd98b4cb3
morozovivan95 <morozov.ivan95@yandex.ru>
2016-12-23 03:47:40 +0300

    окончательно поправил роут коннекта игрока

--- src/main/java/elections/controller/GameController.java
+++ src/main/java/elections/controller/GameController.java
@@ -5,18 +5,18 @@ import elections.domain.State;
 import elections.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
-@Controller
+@RestController
 public class GameController {
 
     @Autowired
     private GameService gameService;
 
-    @RequestMapping(value = "/cvs/{id}", method = GET)
+    @RequestMapping(value = "/app/start", method = GET)
     public Player startGame() throws Exception {
         return gameService.startGame();
     }
